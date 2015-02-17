b04e8c15a394258836a95c71ec62ba865ac3241f
Vanndy <Vanndy>
2015-02-17 15:58:32 +0700

    update index from tmp branch

--- index.php
+++ index.php
@@ -5,5 +5,7 @@ echo 'Click to see your profile...';
 echo 'We have delete profile page';
 echo 'Git reset HEAD';
 echo 'Adding profile page';
+echo 'Play with git stash';
+echo 'and upate stash from tmp branch';
 ?>
 <a href="home/home.php">Visit home page<a>
