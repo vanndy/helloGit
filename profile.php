f3427f7226b315904535053de8bf39e2f0843590
Vanndy <Vanndy>
2015-02-17 15:25:18 +0700

    update profile page

--- profile.php
+++ profile.php
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 Hello profile page
 
 Update profile testing for GIT Stash -- Update
-=======
+
 <h2>Hello profile page</h2>
 <p>Update profile page on b_branch</p>
-
->>>>>>> b_profile
